42677834c11d6d8bb5affff5dd62ffaa72331736
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-09-24 20:20:35 +0530

    added get request method

--- src/main/java/com/example/demorestapicalls/controller/HelloController.java
+++ src/main/java/com/example/demorestapicalls/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.example.demorestapicalls.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +13,10 @@ public class HelloController {
 	@RequestMapping(value = {"","/","/home"})
 	public String hello() {
 		return "Hello !";
+	}
+	
+	@RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+	public String sayHello(@RequestParam(value = "name") String name) {
+		return "Hello " + name + "!";
 	}
 }
