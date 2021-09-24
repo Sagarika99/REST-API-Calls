bfee2f476f02b24e3bcdd53faa3403cc8f9ff356
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-09-24 21:06:41 +0530

    added post method

--- src/main/java/com/example/demorestapicalls/controller/HelloController.java
+++ src/main/java/com/example/demorestapicalls/controller/HelloController.java
@@ -1,11 +1,16 @@
 package com.example.demorestapicalls.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demorestapicalls.model.User;
 
 @RestController
 @RequestMapping("/hi")
@@ -24,5 +29,10 @@ public class HelloController {
 	@GetMapping("/param/{name}")
 	public String sayHelloParam(@PathVariable String name) {
 		return "Hello " + name + "!";
+	}
+	
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user) {
+		return "Hello " + user.getFirstName() + " " + user.getLastName() + "!";
 	}
 }
