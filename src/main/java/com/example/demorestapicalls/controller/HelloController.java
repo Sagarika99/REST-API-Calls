3e16f912a17b15d08dd67ecaa877c101c5d7fb29
Sagarika Shinde <sagarika.shinde@morningstar.com>
2021-09-24 20:41:24 +0530

    added get method and path variable

--- src/main/java/com/example/demorestapicalls/controller/HelloController.java
+++ src/main/java/com/example/demorestapicalls/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demorestapicalls.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +18,11 @@ public class HelloController {
 	
 	@RequestMapping(value = {"/query"}, method = RequestMethod.GET)
 	public String sayHello(@RequestParam(value = "name") String name) {
+		return "Hello " + name + "!";
+	}
+	
+	@GetMapping("/param/{name}")
+	public String sayHelloParam(@PathVariable String name) {
 		return "Hello " + name + "!";
 	}
 }
